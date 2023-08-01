72c61adb93cd31ee747b85973889442344160f40
zhaogongchengsi <zzh1586169624@163.com>
2023-08-01 14:41:45 +0800

    change: 啥都没改

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { loadConfig, defineBriskConfig } from "./config";
 export { convert, transform } from "./transform";
 export { build } from './build'
+export { Features } from 'lightningcss'
